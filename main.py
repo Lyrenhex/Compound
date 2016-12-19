f5c2c3bb87b80e92a86d55e9381eb529ce3d0a74
Damian Heaton <dh64784@gmail.com>
2016-12-19 19:37:06 +0000

    Add verbose transclusion, supporting auto-insertion of css/js
    -v or --verbose

--- main.py
+++ main.py
@@ -45,19 +45,55 @@ if __name__ == "__main__":
             output = sys.argv[sys.argv.index("-o") + 1]
         elif "--output" in sys.argv:
             output = sys.argv[sys.argv.index("--output") + 1]
+
+        if "-b" in sys.argv:
+            output = sys.argv[sys.argv.index("-b") + 1] + output
+        elif "--build-dir" in sys.argv:
+            output = sys.argv[sys.argv.index("-b") + 1] + output
+
         try:
             if(".c.html" not in sys.argv[1]):
                 print("WARNING: CHTML input file does not contain '.c.html' in its name; are you sure that it's the correct format?")
             chtml = bs4.BeautifulSoup(open(sys.argv[1]), "html.parser")
+
+            # index all of the tags that we deal with
             transcludes = chtml.find_all("link", rel="transclusion")
+            csslinks = chtml.find_all("link", rel="stylesheet")
+            scriptlinks = chtml.find_all("script", src=True)
+
             for transclusion in transcludes:
                 try:
-                    transclusion.replaceWith(bs4.BeautifulSoup(open(transclusion["href"])))
+                    transclusion.replaceWith(bs4.BeautifulSoup(open(transclusion["href"]), "html.parser"))
                 except IOError as e:
                     print("Transclusion failure: file %s could not be found -- perhaps it's misspelt?" % transclusion["href"])
                     del transclusion
                 except Exception as e:
                     print("Transclusion failure:", e)
+
+            if "-v" in sys.argv or "--verbose" in sys.argv:
+                # they passed the verbose flag; embed script and css files
+
+                # css files
+                for cssfile in csslinks:
+                    # create a new <style> tag to contain embedded css
+                    csstag = chtml.new_tag("style")
+
+                    # fill the <style> tag with the css
+                    csstag.append(bs4.BeautifulSoup(open(cssfile["href"]), "html.parser"))
+
+                    # replace the link tag with the <style> tag
+                    cssfile.replaceWith(csstag)
+
+                # script files
+                for scriptfile in scriptlinks:
+                    # create a new <script> tag to contain embedded js
+                    scripttag = chtml.new_tag("script")
+
+                    # fill the <script> tag with the js
+                    scripttag.append(bs4.BeautifulSoup(open(scriptfile["src"]), "html.parser"))
+
+                    # replace the link tag with the <script> tag
+                    scriptfile.replaceWith(scripttag)
 
             if minify:
                 chtml = htmlmin.minify(str(chtml), remove_empty_space=True, remove_comments=nocomm)
